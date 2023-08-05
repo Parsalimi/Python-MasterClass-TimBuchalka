0810a5ea9912aea0449ceb3ca72db943a623eef0
Parsa Salimi <salimi1382@gmail.com>
2023-08-05 13:05:27 +0330

    Condition

---
+++ SS_04-011-condition.py
@@ -0,0 +1,16 @@
+age = int(input("Please enter your age? "))
+
+# if age >= 18 and age <= 70:
+#     print("You've to WORK!")
+
+if 18 <= age <= 70:
+    print("You've to WORK!")
+else:
+    print("Enjoy your free time!")
+
+print("-"*25)
+
+if age < 18 or age > 70:
+    print("Enjoy your free time!")
+else:
+    print("You've to WORK!")
