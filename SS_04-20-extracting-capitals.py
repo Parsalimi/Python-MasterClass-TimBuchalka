db71f6129a8dec57b505c85ed9919522b6eed2cd
Parsa Salimi <salimi1382@gmail.com>
2023-08-06 14:10:59 +0330

    Class SS_04-020

---
+++ SS_04-20-extracting-capitals.py
@@ -0,0 +1,7 @@
+text = "Alright, but apart from the Sanitation, the Medicine, Education, Wine, Public Order, Irrigation,Roads, the Fresh-Water System, and Public Health, what have the Romans ever done for us?"
+Capitals = ""
+for char in text:
+    if char.isupper():
+        Capitals = Capitals + char
+
+print(Capitals)
