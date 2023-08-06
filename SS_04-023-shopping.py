fa328a50bbcb82751144eea4bf2b2191c612fd58
Parsa Salimi <salimi1382@gmail.com>
2023-08-06 20:23:09 +0330

    Class SS_04-24

--- SS_04-023-shopping.py
+++ SS_04-023-shopping.py
@@ -1,11 +1,18 @@
 shopping_list = ["milk","pasta","eggs","spam","rice","bread"]
 
-for item in shopping_list:
-    if item != "spam":
-        print("Buy " + item)
+# for item in shopping_list:
+#     if item != "spam":
+#         print("Buy " + item)
+#
+# for item in shopping_list:
+#     if item == "spam":
+#         continue
+#
+#     print("Buy " + item)
+
 
 for item in shopping_list:
     if item == "spam":
-        continue
+        break
 
     print("Buy " + item)
