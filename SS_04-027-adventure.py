083b490b78c92e69998f51551c1b29120e321807
Parsa Salimi <salimi1382@gmail.com>
2023-08-06 23:07:30 +0330

    Create SS_04-027-adventure.py

---
+++ SS_04-027-adventure.py
@@ -0,0 +1,7 @@
+availabe_exits = ["north","west","east","south"]
+
+choosen_exit = ""
+while choosen_exit not in availabe_exits:
+    choosen_exit = input("Please choose a directino: ")
+
+print("arent you glad you got out of there")
