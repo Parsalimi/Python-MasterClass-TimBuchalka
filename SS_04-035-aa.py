8f1087f5693e3e36a7c01a68b82ceffe42f5cd25
Parsa Salimi <salimi1382@gmail.com>
2023-08-07 18:13:02 +0330

    Create SS_04-035-aa.py

---
+++ SS_04-035-aa.py
@@ -0,0 +1,31 @@
+#Augmented Assignment
+x = 23
+
+x += 1
+print(x) # 24
+
+x -= 4
+print(x) # 20
+
+x *= 5
+print(x) # 100
+
+x //= 4
+print(x) # 25
+
+x /= 5
+print(x) # 5.0
+
+x **= 2
+print(x) # 25.0
+
+x %= 5
+print(x) # 0.0
+
+greeting = "Good "
+
+greeting += "morning"
+print(greeting) # Good morning
+
+greeting *= 5
+print(greeting) # Good morningGood morningGood morningGood morningGood morning
