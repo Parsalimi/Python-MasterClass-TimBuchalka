503ddb490170b0182b1313b296374c92ba83ff84
Parsa Salimi <salimi1382@gmail.com>
2024-02-24 11:28:04 +0330

    learning tuple and little progress on it!

--- S05/SS_05-033-more_print.py
+++ S05/SS_05-033-more_print.py
@@ -2,5 +2,6 @@ name = 'Parsa'
 age = 20
 
 print(name, age, 'Python', 2024)
-print(name, age, 'Python', 2024, sep=", ") 
+print((name, age, 'Python', 2024)) # Created a Tuple
+#print(name, age, 'Python', 2024, sep=", ")
 
